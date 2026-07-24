af5d61d48f3078615690e5c945bfbdb08fc10ee3
Rakshith <Rakshithgamer123456789@email.com>
2026-07-24 10:56:19 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int numofentries = 0;
+int Dataset[MAX_SIZE];
+
+void checkdatasetsize()
+{
+    if (numofentries == 0)
+    {
+        printf("Your dataset is empty.\n");
+
+        printf("How many elements does this dataset contain?\n");
+        scanf("%d", &numofentries);
+
+        if (numofentries <= 0 || numofentries > MAX_SIZE)
+        {
+            printf("Dataset size must be between 1 and %d.\n", MAX_SIZE);
+            numofentries = 0;
+            return;
+        }
+
+        for (int i = 0; i < numofentries; i++)
+        {
+            printf("Enter data element %d: ", i + 1);
+            scanf("%d", &Dataset[i]);
+        }
+    }
+}
+
+float sum()
+{
+    int Sum = 0;
+
+    for (int i = 0; i < numofentries; i++)
+    {
+        Sum = Sum + Dataset[i];
+    }
+
+    return Sum;
+}
+float avg(){
+    sum();
+    float avg;
+    avg=sum()/numofentries;
+    return avg;
+    
+}
+
+int min(){
+    int Min=Dataset[0];
+    for (int i = 0; i < numofentries; i++)
+    {
+        if (Dataset[i] < Min)
+        {
+            Min = Dataset[i];
+        }
+    }
+    return Min;
+}
+
+int max()
+{
+    int max = Dataset[0];
+    for (int i = 0; i < numofentries; i++)
+    {
+        if (Dataset[i] > max)
+        {
+            max = Dataset[i];
+        }
+    }
+    return max;
+}
+
+int LinearSearch(int value)
+{
+    for (int i = 0; i < numofentries; i++)
+    {
+        if (value == Dataset[i])
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void BubbleSortAscending()
+{
+    for (int i = 0; i < numofentries - 1; i++)
+    {
+        for (int j = 0; j < numofentries - i - 1; j++)
+        {
+            if (Dataset[j] > Dataset[j + 1])
+            {
+                int temp = Dataset[j];
+                Dataset[j] = Dataset[j + 1];
+                Dataset[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void BubbleSortDescending(){
+    for (int i = 0; i < numofentries - 1; i++)
+    {
+        for (int j = 0; j < numofentries - i - 1; j++)
+        {
+            if (Dataset[j] < Dataset[j + 1])
+            {
+                int temp = Dataset[j];
+                Dataset[j] = Dataset[j + 1];
+                Dataset[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int menu;
+    int result;
+    checkdatasetsize();
+    printf("\nThe dataset is:\n");
+
+    for (int i = 0; i < numofentries; i++)
+    {
+        printf("%d ", Dataset[i]);
+    }
+
+    while (1)
+    {
+        printf("\n");
+        printf("--------------- Menu ---------------\n");
+        printf("1. Sum\n");
+        printf("2. Average\n");
+        printf("3. Minimum\n");
+        printf("4. Maximum\n");
+        printf("5. Search\n");
+        printf("6. Bubblesort\n");
+        printf("7. Descending Order\n");
+        printf("8. Exit\n");
+
+        scanf("%d", &menu);
+
+        switch (menu)
+        {
+        case 1:
+            printf("Sum is %.2f\n", sum());
+            break;
+        case 2:
+            printf("average is %.2f\n",avg());
+            break;
+        case 3:
+            printf("Minimum is %d\n",min());
+            break;
+        case 4:
+            printf("Maximum is %d\n", max());
+            break;
+        case 5:{
+            int value;
+            printf("Type your desired value: ");
+            scanf("%d", &value); 
+            result=LinearSearch(value);
+            if (result == -1){
+                printf("Value %d was not found.\n", value);
+            }
+            else{
+                printf("Value %d was found at index %d.\n", value, result);
+            }
+            break;
+        }
+        case 6:
+            BubbleSortAscending();
+            printf("Dataset sorted: ");
+            for (int i = 0; i < numofentries; i++) {
+                printf("%d ", Dataset[i]);
+            }
+            printf("\n");
+            break;
+        case 7:
+            BubbleSortDescending();
+            printf("Dataset sorted: ");
+            for (int i = 0; i < numofentries; i++) {
+                printf("%d ", Dataset[i]);
+            }
+            printf("\n");
+            break;
+        
+        case 8:
+            return 1;
+            break;
+
+        default:
+            printf("Invalid option.\n");
+            break;
+        }
+    }
+}
